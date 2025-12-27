3bfdbae5996b6d4c778ebae91054d43f84110c38
Derek Bruckner <dbrucknr@umich.edu>
2025-12-27 15:13:46 -0500

    Commits working flow for having Rust serve built SolidJS SPA

--- core/src/app/mod.rs
+++ core/src/app/mod.rs
@@ -1,8 +1,11 @@
 // Local Module Registry
 pub mod traits;
 
+use std::path::Path;
+
 // Third Party Library Crates
-use axum::Router;
+use axum::{Router, routing::get_service};
+use tower_http::services::{ServeDir, ServeFile};
 
 // Local Library Crates
 use traits::ApplicationRouter;
@@ -15,8 +18,21 @@ impl Application {
     pub fn new(
         api: impl ApiRouter + 'static,
     ) -> Self {
+        let assets = ServeDir::new("./static/assets");
+        let index = ServeFile::new("./static/index.html");
+
+        let assets_path = "./static/assets";
+        println!(
+            "Serving assets from {:?}, exists: {}",
+            assets_path,
+            Path::new(assets_path).exists()
+        );
+
         Self {
-            router: Router::new().nest("/api", api.router())
+            router: Router::new()
+                .nest("/api", api.router())
+                .nest_service("/assets", assets)
+                .fallback_service(get_service(index))
         }
     }
 }
